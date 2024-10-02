32583c70cbdd6439d07d1a9b19258e22e92dbf96
altynbekova <gulzhanaltynbekova@gmail.com>
2024-10-02 22:17:45 +0700

    first commit

---
+++ AutoPark/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  AutoPark
+//
+//  Created by Nurgali Altynbekov on 02.10.2024.
+//
+
+import UIKit
+
+class Vehicle {
+    let make: String
+    let model: String
+    let year: Int
+    var capacity: Int
+    
+    
+}
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
